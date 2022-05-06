3ae93c2bdbecfb6797ab966a2b6a25712072d2c7
myself11c <myself11c@gmail.com>
2022-05-06 09:48:48 -0500

    correccion value empresa

--- asignar_cita.php
+++ asignar_cita.php
@@ -230,7 +230,7 @@ Navbar
                 <input name="iIdPacienteSios" type="hidden" value="" id="iIdPacienteSios">
                 <input name="regimen" type="hidden" value="" id="regimen">
                 <input name="tipo" type="hidden" value="" id="tipo">
-                <input name="IdEmpresa" type="hidden" value="" id="IdEmpresa">
+                <input name="IdEmpresa" type="hidden" value="<?php echo $IdEmpresa?>" id="IdEmpresa">
             </form>
 
         </div>
